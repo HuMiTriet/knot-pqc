0031f57bf78aa4b6fc17bec7be3624cdf806f10a
Jan Doskočil <jan.doskocil@nic.cz>
2026-02-12 11:09:55 +0100

    conf: dud option "disable-any" in zone and server sections for compatibility

--- src/knot/conf/schema.c
+++ src/knot/conf/schema.c
@@ -259,6 +259,7 @@ static const yp_item_t desc_server[] = {
 	{ C_COMMENT,              YP_TSTR,  YP_VNONE },
 	/* Legacy items.*/
 	{ C_TCP_FASTOPEN,         YP_TBOOL, YP_VNONE, YP_FNONE, { legacy_item } },
+	{ C_DISABLE_ANY,          YP_TBOOL, YP_VNONE, YP_FNONE, { legacy_item } },
 	{ NULL }
 };
 
@@ -550,6 +551,8 @@ static const yp_item_t desc_zone[] = {
 	{ C_TPL,    YP_TREF,   YP_VREF = { C_TPL }, CONF_IO_FRLD_ZONE | CONF_IO_FCAT | CONF_IO_FREV,
 	                       { check_ref } },
 	ZONE_ITEMS(CONF_IO_FRLD_ZONE)
+	/* Legacy items.*/
+	{ C_DISABLE_ANY,          YP_TBOOL, YP_VNONE, YP_FNONE, { legacy_item } },
 	{ NULL }
 };
 
